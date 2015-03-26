b9380cbd1af04fdc71785bc132f7a543805392f4
jam1987 <jam1987@gmail.com>
2015-03-25 23:49:09 -0400

    Creado pagina principal Index.html (solo esqueleto sin CSS

--- store_comparator/stores/views.py
+++ store_comparator/stores/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from models import *
+from django.db.models import Q
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.cache import never_cache
+from django.contrib.sites.models import Site
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.db import IntegrityError
+from django.db import DatabaseError
 
 # Create your views here.
+
+def index(request):
+    return render_to_response('stores/home.html')
